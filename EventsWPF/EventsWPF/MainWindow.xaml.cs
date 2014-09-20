275cb2d26f921deac2ebe65c663e22dab271bb3c
qwaevisz <qwaevisz@gmail.com>
2014-09-20 20:55:11 +0200

    ADD: EventsWPF project

---
+++ EventsWPF/EventsWPF/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventsWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly Random random;
+        private readonly String[] people;
+        private readonly String[] actions;
+        private Process process;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.random = new Random();
+            this.people = new String[]{"Steve", "Joe", "Sue", "Stephen"};
+            this.actions = new String[] { "test", "implement", "review", "repair", "refactor" };
+        }
+
+        private void bStart_Click(object sender, RoutedEventArgs e)
+        {
+            int minimum = Int32.Parse(this.tbMin.Text);
+            int maximum = Int32.Parse(this.tbMax.Text);
+            this.process = new Process(minimum, maximum);
+            this.process.commonEvent += process_commonEvent;
+            this.process.featuredProgressEvent += process_featuredProgressEvent;
+
+            this.process.commonEvent += new EventHandler(this.other_process_commonEvent);
+            this.process.featuredProgressEvent += delegate
+            {
+                MessageBox.Show("From Anonymous method!");
+            };
+
+            this.pbDemo.Minimum = minimum;
+            this.pbDemo.Maximum = maximum;
+            this.bStep.IsEnabled = true;
+            this.bStart.IsEnabled = false;
+        }
+
+        private void process_featuredProgressEvent(double progressPercent, string message)
+        {
+            MessageBox.Show("Featured progress! " + message + " ( " + progressPercent + " % )");
+        }
+
+        private void process_commonEvent(object sender, EventArgs e)
+        {
+            double progressPercent = 0;
+            if (sender is Process)
+            {
+                progressPercent = (sender as Process).getPercent();
+            }
+
+            MessageBox.Show("Common event! Progress: " + progressPercent + " % ");
+        }
+
+        private void other_process_commonEvent(object sender, EventArgs e)
+        {
+            MessageBox.Show("Other common event!");
+        }
+
+        private void bStep_Click(object sender, RoutedEventArgs e)
+        {
+            int step = this.random.Next(20);
+            this.process.step(step, this.getActor(), this.getAction());
+            this.pbDemo.Value += step;
+            if (this.pbDemo.Value == this.pbDemo.Maximum)
+            {
+                MessageBox.Show("Done.", "Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.bStep.IsEnabled = false;
+                this.bStart.IsEnabled = true;
+                this.pbDemo.Value = 0;
+            }
+        }
+
+        private String getActor() {
+            return this.people[this.random.Next(this.people.Length)];
+        }
+
+        private String getAction()
+        {
+            return this.actions[this.random.Next(this.actions.Length)];
+        }
+
+    }
+}
---
+++ EventsWPF/EventsWPF/Process.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsWPF
+{
+    public delegate void FeaturedProgressHandler(double progressPercent, String message);
+
+    public class Process
+    {
+        private const double FIRST_FEATURED_LEVEL_PERCENT = 33;
+        private const double SECOND_FEATURED_LEVEL_PERCENT = 66;
+
+        private readonly int MINSTATE;
+        private readonly int MAXSTATE;
+        private int progress = 0;
+
+        public event EventHandler commonEvent;
+        public event FeaturedProgressHandler featuredProgressEvent;
+
+        public int Progress
+        {
+            get { return this.progress; }
+        }
+
+        public Process(int minState, int maxState)
+        {
+            this.MINSTATE = minState;
+            this.MAXSTATE = maxState;
+            this.progress = minState;
+        }
+
+        public double getPercent()
+        {
+            return ((double)(this.progress - this.MINSTATE) / (double)(this.MAXSTATE - this.MINSTATE)) * 100;
+        }
+
+        public void step(int value, String user, String action)
+        {
+            this.progress += value;
+            if (this.getPercent() > Process.FIRST_FEATURED_LEVEL_PERCENT)
+            {
+                if (this.featuredProgressEvent != null)
+                {
+                    this.featuredProgressEvent(this.getPercent(), "'" + action + "' action occured by " + user);
+                }
+            }
+            if (this.getPercent() > Process.SECOND_FEATURED_LEVEL_PERCENT)
+            {
+                if (this.commonEvent != null)
+                {
+                    this.commonEvent(this, EventArgs.Empty);
+                }
+            }
+        }
+
+    }
+
+}
