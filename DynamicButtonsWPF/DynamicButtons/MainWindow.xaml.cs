b61198abdb77cd87b4d0525c95038c29abb555a0
edvibed <david.bedok@ericsson.com>
2014-09-26 15:55:21 +0200

    ADD: DynamicButtons and GridTest WPF projects

---
+++ DynamicButtonsWPF/DynamicButtons/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DynamicButtons
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 3; i++)
+            {
+                this.mainGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                this.mainGrid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            int number = 1;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int k = 0; k < 3; k++)
+                {
+                    Button button = new Button();
+                    button.Margin = new Thickness(10, 10, 10, 10);
+                    // button.Padding = new Thickness(10, 10, 10, 10);
+                    button.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+                    button.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                    button.Content = number++;
+                    
+                    Grid.SetColumn(button, k);
+                    Grid.SetRow(button, i);
+                    this.mainGrid.Children.Add(button);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ GridTestWPF/GridTest/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GridTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Button button = new Button();
+            button.Margin = new Thickness(0, 0, 0, 0);
+
+            this.mainGrid.Children.Add(button);
+
+        }
+    }
+}
