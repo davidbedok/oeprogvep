82ff1849af5401ff2aa14529e2560e66ec5e4a91
Dávid Bedők <david@bedok.hu>
2015-03-27 21:27:22 +0100

    CHG: constans

---
+++ BindingsWPF/BindingsWPF/Utility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingsWPF
+{
+    public class Utility
+    {
+
+        public const String SCVALUE = "Lorem";
+        public const int ICVALUE = 42;
+
+        public static readonly String SVALUE = "Ipsum";
+        public static readonly int IVALUE = 30;
+
+    }
+}
